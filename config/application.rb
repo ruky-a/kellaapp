76434b737c006c5c9bcda10775221cfa6466cdad
ruky-a <rukevwe2006@gmail.com>
2020-02-19 17:51:16 -0500

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,23 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Kellaapp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+     config.assets.paths << Rails.root.join("app", "assets", "font", "roboto")
+   config.assets.paths << Rails.root.join("vendor", "assets", "img")
+  config.assets.paths << Rails.root.join("vendor", "assets", "mdb-addons")
+   config.assets.paths << Rails.root.join("vendor", "assets", "addons-pro")
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
---
+++ vendor/bundle/ruby/2.6.0/gems/font_awesome5_rails-1.0.0/lib/font_awesome5_rails/version.rb
@@ -0,0 +1,4 @@
+module FontAwesome5Rails
+  FA_VERSION = '5.12.0'
+  VERSION = '1.0.0'
+end
