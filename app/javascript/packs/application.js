001fa500819e81da98a2cc631fda34fe2979aedb
ruky-a <rukevwe2006@gmail.com>
2020-02-19 18:18:24 -0500

    update js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,10 +10,6 @@ require("channels")
 
 import 'bootstrap'
 
-document.addEventListener("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip()
-})
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -22,4 +18,7 @@ document.addEventListener("turbolinks:load", () => {
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+
+require("trix")
+require("@rails/actiontext")
 
