70d8f1fe9eb206be8f6dd4e5272508948bf81a27
ruky-a <rukevwe2006@gmail.com>
2020-02-19 18:10:39 -0500

    Js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,7 +21,5 @@ document.addEventListener("turbolinks:load", () => {
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-require("trix")
-require("@rails/actiontext")
 
 
