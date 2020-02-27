28079bd8f59c01584927e91896c2cb6ee2552e16
ruky-a <rukevwe2006@gmail.com>
2020-02-26 22:46:13 -0500

    Add social sharing

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -17,4 +17,14 @@ class Lesson < ApplicationRecord
     end
     return lesson
   end
+
+  def prev_lesson 
+
+    lesson = section.lessons.where("row_order < ?", self.row_order).rank(:row_order).last
+    if lesson.blank? && section.prev_section
+      return section.prev_section.lessons.rank(:row_order).last
+    end
+    return lesson
+
+  end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -10,6 +10,11 @@ class Section < ApplicationRecord
     return section
 end
 
+  def prev_section
+    section = course.sections.where("row_order < ?", self.row_order).rank(:row_order).last
+    return section
+end
+
 
 
 end
